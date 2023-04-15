7e67b1e898538e610f88d94618be461cb9d0250b
cborja09@icloud.com <cborja09@icloud.com>
2023-04-15 12:37:51 -0700

    Updated Commit

--- src/Class10/Homework3.java
+++ src/Class10/Homework3.java
@@ -2,6 +2,8 @@ package Class10;
 
 public class Homework3 {
     public static void main(String[] args) {
-
+        //Using 2D array create a grocery list
+        //Inside you should have an array of veggies, fruits, dairy and sweets
+        // Retrieve all values from that array using 2 different loops
     }
 }
