98262ec0f0312e1a769c810ca5374a6ced4be484
cborja09@icloud.com <cborja09@icloud.com>
2023-04-15 12:35:58 -0700

    Updated Commit

--- src/Class10/Homework2.java
+++ src/Class10/Homework2.java
@@ -2,6 +2,7 @@ package Class10;
 
 public class Homework2 {
     public static void main(String[] args) {
-
+        //Create 2D array of cars : american, german, korean, italian.
+        //Then retrieve all values from that array using 2 different loops.
     }
 }
