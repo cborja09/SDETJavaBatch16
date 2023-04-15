b6037bdd150514c1b96a0e06fe2d42e0efcbc80c
cborja09@icloud.com <cborja09@icloud.com>
2023-04-15 12:40:50 -0700

    Updated Commit

--- src/Class10/Homework5.java
+++ src/Class10/Homework5.java
@@ -3,6 +3,6 @@ package Class10;
 public class Homework5 {
     public static void main(String[] args) {
         //Create a 2D array or integer type where you will store odd and even numbers in 3 rows and 4 columns.
-        // Develop a program which will identify/print the even numbers only.
+        //Develop a program which will identify/print the even numbers only.
     }
 }
