7b8b803cf667613dbc1c1fc626b30517a8849e7e
cborja09@icloud.com <cborja09@icloud.com>
2023-04-15 12:42:54 -0700

    Updated Commit

--- src/Class10/Homework6.java
+++ src/Class10/Homework6.java
@@ -2,6 +2,7 @@ package Class10;
 
 public class Homework6 {
     public static void main(String[] args) {
-
+        //Create 2D array of countries: North America countries, South America countries, Europe countries, Asian countries, African countries.
+        //Then print all values from that array using 2 different loops and calculate how many total countries been stored.
     }
 }
