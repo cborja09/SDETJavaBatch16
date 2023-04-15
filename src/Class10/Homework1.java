8822e4ba32326263daec104ccfbfe7d43e36c204
cborja09@icloud.com <cborja09@icloud.com>
2023-04-15 12:20:58 -0700

    Updated Commit

--- src/Class10/Homework1.java
+++ src/Class10/Homework1.java
@@ -3,5 +3,7 @@ package Class10;
 public class Homework1 {
     public static void main(String[] args) {
 
+        //Create a 2D array(shorter way) in which first array will consist of 4 names and second array will contain grades.
+        //Then your program should print name of the students that has A and B grad.
     }
 }
