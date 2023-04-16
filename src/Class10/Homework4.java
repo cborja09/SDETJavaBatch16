9f112285c3fda64167b4e99d4e5a1e9f3bacd97e
cborja09@icloud.com <cborja09@icloud.com>
2023-04-15 20:12:32 -0700

    Updated Commit

--- src/Class10/Homework4.java
+++ src/Class10/Homework4.java
@@ -2,6 +2,19 @@ package Class10;
 
 public class Homework4 {
     public static void main(String[] args) {
-        //Create a 2D array or integer type and store numbers in 3 rows and 3 columns. Print the sum of all numbers
+        //Create a 2D array or integer type and store numbers in 3 rows and 3 columns. Print the sum of all numbers.
+
+        int[][] arr = {
+                {9, 9, 1},
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int columns = 0; columns < arr[row].length; columns++) {
+                System.out.print(arr[row][columns]+ " ");
+            }
+            System.out.println();
+        }
     }
 }
