0b9d80bd4f7c5aabb457bde2c2895836cde3b389
cborja09@icloud.com <cborja09@icloud.com>
2023-04-15 19:17:42 -0700

    Updated Commit

--- src/Class10/Homework1.java
+++ src/Class10/Homework1.java
@@ -5,5 +5,6 @@ public class Homework1 {
 
         //Create a 2D array(shorter way) in which first array will consist of 4 names and second array will contain grades.
         //Then your program should print name of the students that has A and B grad.
+
     }
 }
--- src/Class10/Homework6.java
+++ src/Class10/Homework6.java
@@ -4,5 +4,28 @@ public class Homework6 {
     public static void main(String[] args) {
         //Create 2D array of countries: North America countries, South America countries, Europe countries, Asian countries, African countries.
         //Then print all values from that array using 2 different loops and calculate how many total countries been stored.
+
+        String [][] arr={
+                {"USA","Mexico", "Canada"},
+                {"Brazil","Peru"},
+                {"England","France","Germany","Spain","Belgium"},
+                {"Japan","South Korea","Thailand"},
+                {"South Africa","Nigeria","Ghana"}
+        };
+        for (int row = 0; row < arr.length; row++) {
+            for (int columns = 0; columns<arr[row].length; columns++) {
+                System.out.println(arr[row][columns]+" ");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+        //
+        for(String [] countries : arr ){
+            for(String country: countries){
+                System.out.println(country+" ");
+            }
+            System.out.println();
+        }
     }
 }
