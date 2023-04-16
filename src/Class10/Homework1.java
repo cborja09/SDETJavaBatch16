c6181dd43f5488000d7b09f5a0cda845185ac04d
cborja09@icloud.com <cborja09@icloud.com>
2023-04-15 19:58:53 -0700

    Updated Commit

--- src/Class10/Homework1.java
+++ src/Class10/Homework1.java
@@ -6,5 +6,13 @@ public class Homework1 {
         //Create a 2D array(shorter way) in which first array will consist of 4 names and second array will contain grades.
         //Then your program should print name of the students that has A and B grad.
 
+        String[][]names={
+                {"Corey","Gautam","Brian","Gian"},
+                {"A","B","C","D"}
+
+        };
+        System.out.print(names[0][0]+ " " +names[1][0]);
+        System.out.println();
+        System.out.print(names[0][1]+ " " +names[1][1]);
     }
 }
