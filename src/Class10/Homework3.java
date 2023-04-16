d02537452b94161a71dc6c1a1c1a46828932e80d
cborja09@icloud.com <cborja09@icloud.com>
2023-04-15 20:06:06 -0700

    Updated Commit

--- src/Class10/Homework3.java
+++ src/Class10/Homework3.java
@@ -5,5 +5,27 @@ public class Homework3 {
         //Using 2D array create a grocery list
         //Inside you should have an array of veggies, fruits, dairy and sweets
         // Retrieve all values from that array using 2 different loops
+
+        String[][] arr = {
+                {"Asparagus", "Lettuce", "Onions", "Mushrooms"},
+                {"Pineapple", "Apple", "Guava"},
+                {"Milk", "Greek Yogurt"},
+                {"Cake", "Cookies", "Candy"}
+        };
+        // For Loop
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                System.out.print(arr[row][column] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        // Enhanced For Loop
+        for (String[] list : arr) {
+            for (String grocery : list) {
+                System.out.print(grocery + " ");
+            }
+            System.out.println();
+        }
     }
 }
