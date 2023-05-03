180ed0c0e69132257640f185f2caaeadb122cd2d
cborja09@icloud.com <cborja09@icloud.com>
2023-05-02 22:08:15 -0700

    Updated Commit

--- src/Class12/E1StringDemo.java
+++ src/Class12/E1StringDemo.java
@@ -4,13 +4,13 @@ public class E1StringDemo {
     public static void main(String[] args) {
 
         // create the object of that class as step one
-        // Syntax: ClassName objectjectName =new ClassName();
+        // Syntax: ClassName objectName =new ClassName();
         //String s=new String("Ana");
 
         String string1="Ana"; // this syntax approach is the most widely used one
         System.out.println(string1.length()); // There are 3 letters in "Ana". Output would be 3.
 
-        String name="Corey";
+        String name="Coreyyyyy";
 
         if (name.length()>8){
             System.out.println("Only 8 characters are allowed.");
