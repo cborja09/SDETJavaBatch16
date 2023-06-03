b28d2019d41573d35dfa2d73feaf26535c293335
cborja09@icloud.com <cborja09@icloud.com>
2023-06-03 13:10:01 -0700

    Updated Commit

---
+++ src/main/java/Class26/E3Maps.java
@@ -0,0 +1,17 @@
+package Class26;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class E3Maps {
+    public static void main(String[] args) {
+        Map<String, Double> drinks = new HashMap<>();
+
+        drinks.put("Coke",2.0);
+        drinks.put("Milk",5.0);
+        drinks.put("Mango juice",2.5);
+        drinks.put("Coffee",3.0);
+        drinks.put("Tea", 3.5);
+        //remove only those which only contains letter i and their price is less than 3
+    }
+}
