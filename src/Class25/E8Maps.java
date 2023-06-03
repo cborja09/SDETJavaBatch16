f39f945388d0a3b072c6064a8df6f53e69850f9c
cborja09@icloud.com <cborja09@icloud.com>
2023-06-02 19:26:10 -0700

    Updated Commit

--- src/Class25/E8Maps.java
+++ src/Class25/E8Maps.java
@@ -6,6 +6,15 @@ public class E8Maps {
     public static void main(String[] args) {
 
         LinkedHashMap<Integer, String> ranks=new LinkedHashMap<>();
+        ranks.put(1,"James");
+        ranks.put(2,"Adam");
+        ranks.put(3,"John");
+        ranks.put(4,"Jimmy");
+        System.out.println(ranks);
+        ranks.remove(2); //removes Adam
+        System.out.println(ranks);
+        ranks.replace(1,"Adam"); //replaces James for Adam in key 1
+        System.out.println(ranks);
 
     }
 }
---
+++ src/Class25/E9Maps.java
@@ -0,0 +1,21 @@
+package Class25;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class E9Maps {
+    public static void main(String[] args) {
+
+        TreeMap<String,Double> makeup=new TreeMap<>();
+        makeup.put("Lipstick",100.0);
+        makeup.put("Blush",68.0);
+        makeup.put("Eye Liner",80.0);
+        makeup.put("Foundation",120.0);
+        makeup.put("Base",25.0);
+        Set<String> keys= makeup.keySet();
+        System.out.println(keys);
+        Collection<Double> values = makeup.values();
+        System.out.println(values);
+    }
+}
