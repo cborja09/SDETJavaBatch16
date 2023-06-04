df15aa7a518378a7475f2f28f04806d6ab3d17c1
cborja09@icloud.com <cborja09@icloud.com>
2023-06-03 20:48:59 -0700

    Updated Commit

--- src/main/java/Class26/E4ConfigFiles.java
+++ src/main/java/Class26/E4ConfigFiles.java
@@ -1,10 +1,20 @@
 package Class26;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class E4ConfigFiles {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //System.property("user.dir") gives the path of the project
         System.out.println(System.getProperty("user.dir"));
-        String path=System.getProperty("user.dir")+"Files/Config.properties";
+        //For Mac users
+        String path=System.getProperty("user.dir")+"/"+"Files/Config.properties";
         System.out.println(path);
+        FileInputStream fileInputStream=new FileInputStream(path);
+        //That special class which knows how this file works
+        Properties properties=new Properties();
+        properties.load(fileInputStream);
+        System.out.println(properties.getProperty("userName"));
     }
 }
