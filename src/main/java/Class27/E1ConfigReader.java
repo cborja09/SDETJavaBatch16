480c10aee81bdacfa6dacd99da02a10240698c00
cborja09@icloud.com <cborja09@icloud.com>
2023-06-04 15:10:39 -0700

    Updated Commit

---
+++ src/main/java/Class27/E1ConfigReader.java
@@ -0,0 +1,11 @@
+package Class27;
+    /*
+    Get the location of the file
+    Either navigate to that file or bring that file into the memory
+     */
+
+public class E1ConfigReader {
+    public static void main(String[] args) {
+
+    }
+}
