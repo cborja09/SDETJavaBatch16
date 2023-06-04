964fbe4e99ed3539c9e17b91fe7b5215cbf11e37
cborja09@icloud.com <cborja09@icloud.com>
2023-06-03 21:45:26 -0700

    Updated Commit

---
+++ src/main/java/Class26/E5ConfigReader.java
@@ -0,0 +1,12 @@
+package Class26;
+
+import Utils.ConfigReader;
+
+import java.io.IOException;
+
+public class E5ConfigReader {
+    public static void main(String[] args) throws IOException {
+        System.out.println(ConfigReader.getProperty("URL"));
+
+    }
+}
