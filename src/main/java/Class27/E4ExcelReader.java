7f4523110d02ea7731292d26b59cf4987edd3159
cborja09@icloud.com <cborja09@icloud.com>
2023-06-04 19:39:38 -0700

    Updated Commit

---
+++ src/main/java/Class27/E4ExcelReader.java
@@ -0,0 +1,32 @@
+package Class27;
+
+import Utils.Constants;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class E4ExcelReader {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(Constants.EXCEL_FILE_PATH);
+        //that special class which knows how to read the data from Excel files
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
+        Sheet sheet = xssfWorkbook.getSheet("Sheet1");
+        for (int rows = 0; rows < 4; rows++) {
+            Row row = sheet.getRow(rows);
+            for (int col = 0; col < 5; col++) {
+                System.out.print(row.getCell(col) + " ");
+            }
+            System.out.println();
+        }
+
+        /*System.out.print(row.getCell(0)+" ");
+        System.out.print(row.getCell(1)+" ");
+        System.out.print(row.getCell(2)+" ");
+        System.out.print(row.getCell(3)+" ");
+        System.out.print(row.getCell(4));
+        */
+    }
+}
